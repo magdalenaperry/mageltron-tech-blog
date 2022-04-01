4e40bfd3bdadd18a4e0fcaf5d342759907374a2f
Magdalena Perry <magperry16@gmail.com>
2022-04-01 18:32:06 -0400

    create models

---
+++ models/comment.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model{
+
+}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true
+    }, 
+    comment_body: {
+      type:DataTypes.STRING, 
+      allowNull: false, 
+      validate: {
+        len:[1, 255]
+      }
+    }, 
+    user_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'user', 
+        key: 'id'
+      }
+    }, 
+    post_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'post', 
+        key: 'id'
+      }
+    }
+  }, 
+  {
+    sequelize, 
+    // timestamp: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+)
+
+module.exports = Comment; 
